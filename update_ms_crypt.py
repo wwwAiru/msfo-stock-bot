316f9e65fbe44458425d4dbb731383f3a4025edd
Andrey <92391770+wwwAiru@users.noreply.github.com>
2021-11-14 00:54:32 +0300

    Скрипт для заполнения БД из api coinmarketcap
    Скрипт может добавить все криптовалюты в БД. Для этого нужно запускать его 2 раза, и менять параметр strat для пагинации. Однако я заметил что многие криптовалюты имеют нулевую капитализацию или неопределяемую, это признак мёртвых токенов, поэтому после заполнения базы, автоматически удаляются записи, где капитализация = 0.

---
+++ update_ms_crypt.py
@@ -0,0 +1,42 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+import sqlite3
+
+
+
+
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+#тут указывается лимит количества результатов(больше 5000 нельзя) для пагинации меняется параметр start
+parameters = {
+        'start': '1',
+        'limit': '5000',
+        'convert': 'USD'
+}
+headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': '02925efd-b47c-4000-a468-5078c3ed6c9b',
+}
+session = Session()
+session.headers.update(headers)
+response = session.get(url, params=parameters)
+data = json.loads(response.text)['data']
+
+
+conn = sqlite3.connect('ms.db3')
+cur = conn.cursor()
+#cur.execute('DELETE FROM crypt;')
+"""for x in data:
+    cur.execute('''INSERT or IGNORE INTO crypt(id, name, symbol, slug, price, market_cap, volume_24h, percent_change_24h)
+                         VALUES(?, ?, ?, ?, ?, ?, ?, ?)''',
+                (x["id"], x["name"], x["symbol"], x["slug"], x["quote"]["USD"]["price"],
+                 x["quote"]["USD"]["market_cap"],
+                 x["quote"]["USD"]["volume_24h"],
+                 x["quote"]["USD"]["percent_change_24h"]))"""
+cur.execute('DELETE FROM crypt WHERE market_cap = 0 ;')
+conn.commit()
+conn.close()
+
+print("Done")
+
