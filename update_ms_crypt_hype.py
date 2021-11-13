e98787cbca6d731e369a2c4399adfe73ed7b08f4
Andrey <92391770+wwwAiru@users.noreply.github.com>
2021-11-14 01:01:18 +0300

    Скрипт - функция заполнения таблицы crypt_hype
    Таблица crypt_hype заполняется 200 записями(это почти топ 200 первых криптовалют, но не совсем, токены "фиата" портят порядок, не критично). Можно настраивать количество, и заполнять хоть 5000 тысячами записей, но для определения самых активных по торгам дальше 200 смысла отбирать уже нет, там начинается трэш-токены, а после 1500-2000 монеты с неопределяемой или 0 капитализацией.

---
+++ update_ms_crypt_hype.py
@@ -0,0 +1,47 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+import sqlite3
+
+
+
+
+def update_crypt_hype():
+
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    #тут указывается лимит количества результатов(больше 5000 нельзя) для пагинации меняется параметр start
+    parameters = {
+            'start': '1',
+            'limit': '200',
+            'convert': 'USD'
+    }
+    headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': '02925efd-b47c-4000-a468-5078c3ed6c9b',
+    }
+    session = Session()
+    session.headers.update(headers)
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)['data']
+
+    try:
+        conn = sqlite3.connect('ms.db3')
+        cur = conn.cursor()
+        cur.execute('DELETE FROM crypt_hype;')
+        for x in data:
+            cur.execute('''INSERT or IGNORE INTO crypt_hype(id, name, symbol, price, market_cap, volume_24h, percent_change_24h)
+                             VALUES(?, ?, ?, ?, ?, ?, ?)''',
+                    (x["id"], x["name"], x["symbol"], x["quote"]["USD"]["price"],
+                     x["quote"]["USD"]["market_cap"],
+                     x["quote"]["USD"]["volume_24h"],
+                     x["quote"]["USD"]["percent_change_24h"]))
+        cur.execute('DELETE FROM crypt_hype WHERE market_cap = 0;')
+        conn.commit()
+    except sqlite3.Error as error:
+        print(err)
+    finally:
+        if (conn):
+            conn.close()
+            print("Соединение с SQLite закрыто.")
+    print("Таблица crypt_hype обновлена.")
+
