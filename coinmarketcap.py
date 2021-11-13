4120dac9fa9b2479f5a87623246451c70dab730b
Andrey <92391770+wwwAiru@users.noreply.github.com>
2021-11-14 00:45:33 +0300

    Добавил поле капиталицация
    ещё немного подправил форматирование цены с большим количеством нулей

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -9,27 +9,26 @@ from decimal import Decimal, getcontext
 
 
 
-#открываем файл для чтения в список
+#открываем файл
 with open('cryptlist.txt', 'r', encoding="utf-8") as f:
     crypt_file = f.read()
 #преобразуем файл в список
 crypt_list = crypt_file.split(', ')
-#значение precision в getcontext устанавливает лимит округления для decimal
+#устанавливаем контекст преобразования для Decimal в 15 знаков
 getcontext().prec = 15
 
 def coin_request(user_msg):
-#сравниваем пользовательский месадж со списком
+    #сравниваем пользовательский месадж со списком
     corrected_query = process.extractOne(user_msg, crypt_list)
     print(corrected_query)
-#получаем кортеж с самым совпадающим значением и процентом совпадения
-#используем для запроса в бд чтобы получиь id
+    #получаем кортеж с самым совпадающим значением и процентом совпадения
+    #используем для запроса в бд чтобы получиь id
     try:
         connct = sqlite3.connect('ms.db3')
-#преобразуем курсор для того чтобы получать значения в виде простого списка        
         connct.row_factory = lambda cursor, row: row[0]
         curs = connct.cursor()
-#запрос по тикеру или имени крипты после правки неточности        
-        curs.execute(f'SELECT id FROM crypt WHERE symbol="{corrected_query[0]}" OR name="{corrected_query[0]}";') 
+        # запрос по имени крипты, после правки возможной неточности или ошибки пользовательского ввода
+        curs.execute(f'SELECT id FROM crypt WHERE symbol="{corrected_query[0]}" OR name="{corrected_query[0]}";')
         db_id = curs.fetchone()
     except sqlite3.Error as error:
         print(error)
@@ -37,8 +36,8 @@ def coin_request(user_msg):
         if (connct):
             connct.close()
             print("Соединение с SQLite закрыто")
+    print(db_id)
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-#полученный из бд id подставляем в параметры api запроса    
     parameters = {
         'id': db_id,
     }
@@ -49,10 +48,11 @@ def coin_request(user_msg):
     session = Session()
     session.headers.update(headers)
     response = session.get(url, params=parameters)
-#получаем данные в json виде и получаем доступ к ним по ключам    
-    data = json.loads(response.text)['data'][str(db_id)]    
+    data = json.loads(response.text)['data'][str(db_id)]
     number = Decimal(data['quote']['USD']['price'])
-    if number < 0.01:
+    if number < 0.000001:
+        retrr = number.quantize(Decimal("0.000000000001"))
+    elif number < 0.01:
         retrr = number.quantize(Decimal("0.00000001"))
     elif number < 0.5:
         retrr = number.quantize(Decimal("0.0001"))
@@ -60,8 +60,9 @@ def coin_request(user_msg):
         retrr = number.quantize(Decimal("0.01"))
 
 #делаем человекочитаемый вывод данных. в суточный объём торгов добавил разделитель-запятую т.к. числа большие
-    return f"""{data['name']} ({data['symbol']}) \nЦена: {retrr}$
+    return f"""{data['name']} ({data['symbol']}) Цена: {retrr}$
+    \nКапитализация: {round(data['quote']['USD']['market_cap']):,}$
     \nСуточный объём торгов: {round(data['quote']['USD']['volume_24h']):,}$
-    \nИзменение цены за сутки {round(data['quote']['USD']['percent_change_24h'],2)}%"""
+    \nИзменение цены за сутки: {round(data['quote']['USD']['percent_change_24h'],2)}%"""
 
 
